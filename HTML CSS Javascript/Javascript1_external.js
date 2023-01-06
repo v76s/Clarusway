6008c4acc9409738d82c6dcc1ae77561fa31c94e
volkan <volkansisalan@gmail.com>
2023-01-06 10:43:26 +0300

    Updates

---
+++ HTML CSS Javascript/Javascript1_external.js
@@ -0,0 +1,7 @@
+var age = prompt("Please enter your age ...", "15");
+document.getElementById("output").innerHTML =
+  "You have entered " + age + " for the age prompt";
+console.log(age); /* Display to the console */
+alert(age);
+window.alert(age);
+document.write(age);
