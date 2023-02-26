ca29149e794e9b7488b038b1f290d34ac3e8fc25
v76s <volkansisalan@gmail.com>
2023-02-26 15:09:31 +0300

    Telnet

---
+++ Network/Telnet.py
@@ -0,0 +1,23 @@
+import getpass
+import telnetlib
+
+# https://www.tutorialspoint.com/python_network_programming/python_telnet.htm
+# Putty connection.
+# 
+
+HOST = "http://54.87.158.38/"
+user = input("Enter your remote account: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until("login: ")
+tn.write(user + "\n")
+if password:
+    tn.read_until("Password: ")
+    tn.write(password + "\n")
+
+tn.write("ls\n")
+tn.write("exit\n")
+
+print(tn.read_all())
